2f6cbf9964b2fa922b0f918b77f6faa85cef31a7
Justin Daniel <justinddaniel@gmail.com>
2017-10-31 20:34:37 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,8 @@ class ApplicationController < Sinatra::Base
   #  set :public_folder, 'public'
   #  set :views, 'app/views'
     set :views, Proc.new { File.join(root, "../views/") }
-    enable :sessions unless test?
-    set :session_secret, "secret"
+  #  enable :sessions unless test?
+  #  set :session_secret, "secret"
   end
 
   get '/' do
