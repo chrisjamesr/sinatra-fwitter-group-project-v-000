b7268568115f1d992fd71fcf2ad941543cfcd513
Justin Daniel <justinddaniel@gmail.com>
2017-10-31 21:56:16 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,11 +28,17 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/login' do
-
+    erb :'/users/login'
   end
 
   post '/login' do
-
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/account"
+    else
+    redirect "/failure"
+    end
   end
 
 end
